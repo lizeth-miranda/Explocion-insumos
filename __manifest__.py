50643a052e13b2184921d978f2fa017d2c7874d2
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2021-03-12 17:07:01 -0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Exploción Insumos',
+    'version': '13.1',
+    'author': 'Demsa',
+    'website': '',
+    'depends': [
+        'sale_management',
+        'purchase',
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        # 'views/sale_order.xml',
+        # 'views/account_analytic_line.xml',
+        # 'views/account_move_line.xml',
+        'views/account_analytic_account.xml',
+        'views/purchase_requisition_line.xml',
+        'views/product_template.xml',
+    ],
+}
